48ebbcc1443875899cdc1e261656fc5d6f40ef36
Ryan-Fue <ryanfue@ucla.edu>
2026-05-09 22:48:24 -0700

    Added batch processing

--- 04_protein_embedding/embedder.py
+++ 04_protein_embedding/embedder.py
@@ -1,10 +1,10 @@
 
-from transformers import AutoTokenizer, EsmForMaskedLM
+from transformers import AutoTokenizer, EsmModel
 import torch
 import csv
 import pandas as pd
 import numpy as np
-import tqdm
+from tqdm import tqdm
 
 
 class ESMEmbedder:
@@ -16,28 +16,47 @@ class ESMEmbedder:
         """
 
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-        self.model = EsmForMaskedLM.from_pretrained(model_name)  
+        self.model = EsmModel.from_pretrained(model_name)  
         
 
 
-    def _get_mean_embedding(self, sequence: str) -> np.ndarray:
+    def _get_mean_embedding(self, sequences: list[str]) -> np.ndarray:
 
         """
         Generates the mean embeddings of a protein sequence
         """
 
-
-        inputs = self.tokenizer(sequence, return_tensors="pt", padding=True, truncation=True)
+        inputs = self.tokenizer(sequences, return_tensors="pt", padding=True, truncation=True)
         with torch.no_grad():
             outputs = self.model(**inputs, output_hidden_states=True)
             last_hidden_state = outputs.hidden_states[-1]
             attention_mask = inputs["attention_mask"]
-            embedding = (last_hidden_state * attention_mask.unsqueeze(-1)).sum(1) / attention_mask.sum(1).unsqueeze(-1)
-            return embedding.squeeze(0).numpy()
 
-    def proccess_dataframe(self, df: pd.DataFrame, seq_col: str) -> pd.DataFrame:
+
+            embedding = (last_hidden_state * attention_mask.unsqueeze(-1)).sum(1) / attention_mask.sum(1).unsqueeze(-1)
+            return embedding.cpu().numpy()
+
+    def proccess_dataframe(self, df: pd.DataFrame, seq_col: str, batch_size: int = 32) -> pd.DataFrame:
+
+
+        """
+        Batch process protein sequences for embedding
+        """
+
+        sequences = df[seq_col].tolist()
+        all_embeddings = []
+
+        print(f"Processing {len(sequences)} sequences in batches of {batch_size}...")
+
+        for i in tqdm(range(0, len(sequences), batch_size)):
         
-        df["embedding"] = df[seq_col].apply(self._get_mean_embedding)
+            batch_seqs = sequences[i : i + batch_size]
+
+            batch_embeddings = self._get_mean_embedding(batch_seqs)
+
+            all_embeddings.extend(batch_embeddings)
+
+        df["embeddings"] = all_embeddings
 
         return df
 
